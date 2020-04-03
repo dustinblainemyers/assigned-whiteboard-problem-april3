18c49520082d8134dc5f963f8607529812186088
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-03 09:35:14 -0400

    fizzbuzz complete

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+function fizzBuzz(n) {
+
+    for (let number = 0; number < n; number++) {
+      if(number % 3 === 0 && number % 5 == 0) {
+        console.log("FizzBuzz")
+       } else if(number % 3 === 0) {
+        console.log("Fizz")
+  
+    } else if(number % 5 === 0){
+  
+      console.log("Buzz")
+    } else {
+      console.log(number);
+  
+    }
+  }
+  
+  }
+  
+  fizzBuzz(150);
