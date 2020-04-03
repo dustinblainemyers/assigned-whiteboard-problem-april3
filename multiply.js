e8095228fac2e1397fb731b16b1b67bf6aab1bac
Dustin Myers <dustinblaine.myers@gmail.com>
2020-04-03 09:49:42 -0400

    problem number 2 complete

---
+++ multiply.js
@@ -0,0 +1,17 @@
+//  2. Write a function that multiples two numbers (provided as arguments) without using *
+
+
+function multiply(num1,num2) {
+    let multiplied = num1
+    for (let count= 2; count <= num2; count++) {
+      multiplied = multiplied + num1
+      
+    }
+ 
+   
+     
+ }
+ 
+ 
+ multiply(5,10);
+ 
